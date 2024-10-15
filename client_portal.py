6ed369fff2f502774c3f09e170bdf867cf0f5a45
eugenevanzyl <eugevanz@gmail.com>
2024-10-15 06:30:53 +0200

    Time to add details to client page

--- client_portal.py
+++ client_portal.py
@@ -1,7 +1,8 @@
+import random
 from datetime import datetime
 
 from fasthtml.components import Div, Nav, Ul, Li, A, Span, P, H3, Img, Tr, Td, Tbody, Th, Thead, \
-    Table
+    Table, H2, Caption
 
 from interface import calendar_view
 
@@ -74,13 +75,13 @@ def overview_card():
                     Li(
                         A(
                             Span(data_uk_pagination_previous=True, cls='uk-margin-small-right'),
-                            'Previous',
+                            'September',
                             href='#'
                         )
                     ),
                     Li(
                         A(
-                            'Next',
+                            'November',
                             Span(data_uk_pagination_next=True, cls='uk-margin-small-left'),
                             href='#'
                         ),
@@ -89,6 +90,78 @@ def overview_card():
                     cls='uk-pagination', data_uk_margin=True
                 ),
                 cls='uk-margin-medium-top'
+            ),
+            cls='uk-card-footer'
+        ),
+        cls='uk-card uk-card-default uk-light', style='background-color: #172031;'
+    )
+
+
+def portfolio_value_card():
+    return Div(
+        Div(
+            Div(
+                Div('Portfolio Value', cls='uk-text-default uk-text-bolder'),
+                A('US Dollar', Span(data_uk_drop_parent_icon=True), cls='uk-link-muted uk-text-small'),
+                Div(
+                    Ul(
+                        Li(A('US Dollar', cls='uk-link-muted uk-text-small')),
+                        Li(A('ZA Rand', cls='uk-link-muted uk-text-small')),
+                        Li(A('EURO', cls='uk-link-muted uk-text-small')),
+                        Li(A('British Pound', cls='uk-link-muted uk-text-small')),
+                        cls='uk-list uk-list-divider'
+                    ),
+                    cls='uk-card uk-card-body', style='background-color: #2A3A58;', data_uk_dropdown=True
+                ),
+                cls='uk-flex uk-flex-between'
+            ),
+            cls='uk-card-header'
+        ),
+        Div(
+            Div('Balance', cls='uk-text-small'),
+            H2('R8,167,514.57', cls='uk-text-bolder uk-margin-remove-top uk-margin-remove-bottom uk-text-truncate'),
+            Div('Compared to last month ', Span('+24.17%', cls='uk-text-success'),
+                cls='uk-text-small uk-margin-remove-top'),
+            Div(
+                Div(
+                    Div(Div('142M'), Div('71M', cls='uk-margin-auto-vertical'), Div('0M'),
+                        cls='uk-flex uk-flex-column uk-flex-between uk-text-bolder', style='font-size: 8px;'),
+                    cls='uk-width-auto'
+                ),
+                Div(
+                    Table(
+                        Caption('Portfolio Value'),
+                        Thead(
+                            Tr(
+                                *[Th(title, scope='col') for title in ['Month', 'Income', 'Expenses']]
+                            )
+                        ),
+                        Tbody(
+                            *[Tr(
+                                Th(x['month'], scope='row'),
+                                *[Td(style=f'--size: 0.{amount * 2}') for amount in x['data']]
+                            ) for x in [
+                                dict(month='Jul', data=[random.randint(1, 142), random.randint(1, 142)]),
+                                dict(month='Aug', data=[random.randint(1, 142), random.randint(1, 142)]),
+                                dict(month='Sep', data=[random.randint(1, 142), random.randint(1, 142)]),
+                                dict(month='Oct', data=[random.randint(1, 142), random.randint(1, 142)])
+                            ]]
+                        ),
+                        cls='charts-css column multiple show-data-on-hover show-3-secondary-axes data-spacing-10'
+                    )
+                ),
+                data_uk_grid=True, cls='uk-grid-divider uk-child-width-expand uk-grid-match uk-grid-small '
+                                       'uk-margin-medium-top'
+            ),
+            Div(Div('Jul'), Div('Aug'), Div('Sep'), Div('Oct'),
+                cls='uk-flex uk-flex-around uk-text-bolder uk-text-small uk-margin-small', style='padding-left: 54px;'),
+            cls='uk-card-body'
+        ),
+        Div(
+            Ul(
+                *[Li(title, Span(amount, cls='uk-text-bolder uk-margin-small-left'))
+                  for title, amount in [('Income', '$31,885'), ('Expenses', '$8,994')]],
+                cls='charts-css legend legend-inline legend-square', style='border: 0'
             ),
             cls='uk-card-footer'
         ),
@@ -116,27 +189,108 @@ def transactions():
                     Tr(
                         Th('Type'),
                         Th('Amount'),
-                        Th('Status'),
                         Th('Method')
                     )
                 ),
                 Tbody(
                     *[Tr(
                         Td(
-                            Span(uk_icon='icon:  plus-circle; ratio: 2', cls='uk-text-success'),
+                            Span(
+                                uk_icon=f'icon:  {"plus-circle" if tx_type[0] == "Received" else "minus-circle"}; ratio: 1.5',
+                                cls=f'uk-text-{"success" if tx_type[0] == "Received" else "danger"}'),
                             Div(
                                 Div(title, cls='uk-text-bolder'),
-                                Div('Sent • Aug 24 2024', cls='uk-text-small'),
+                                Div(f'{tx_type[0]} • {tx_type[1]}', cls='uk-text-small'),
                                 cls='uk-margin-small-left'
                             ),
                             cls='uk-flex uk-flex-middle'
                         ),
-                        Td('Table Data'),
-                        Td('Table Data'),
-                        Td('Table Data')
-                    ) for title in ['Company', 'Revenue', 'Bonus', 'Dog food']]
+                        Td(Div(f'R {amount[0]}', cls='uk-text-bolder'), Div(f'R {amount[1]}', cls='uk-text-small')),
+                        Td(Div(method[0], cls='uk-text-bolder'), Div(method[1], cls='uk-text-small'))
+                    ) for title, tx_type, amount, method in [
+                        ('Company', ('Sent', 'Aug 24 2024'), (1500.00, 1371.81), ('Credit Card', '**** 3560')),
+                        ('Revenue', ('Received', 'Aug 24 2024'), (1500.00, 1371.81), ('Bank Transfer', '**** 3560')),
+                        ('Bonus', ('Received', 'Aug 24 2024'), (1500.00, 1371.81), ('Credit Card', '**** 3560')),
+                        ('Dog food', ('Sent', 'Aug 24 2024'), (1500.00, 1371.81), ('Bank Transfer', '**** 3560'))
+                    ]]
                 ),
                 cls='uk-table uk-table-divider'
+            ),
+            cls='uk-card-body'
+        ),
+        Div(
+            Nav(
+                Ul(
+                    Li(
+                        A(
+                            Span(data_uk_pagination_previous=True, cls='uk-margin-small-right'),
+                            'Previous',
+                            href='#'
+                        )
+                    ),
+                    Li(
+                        A(
+                            'Next',
+                            Span(data_uk_pagination_next=True, cls='uk-margin-small-left'),
+                            href='#'
+                        ),
+                        cls='uk-margin-auto-left'
+                    ),
+                    cls='uk-pagination', data_uk_margin=True
+                )
+            ),
+            cls='uk-card-footer'
+        ),
+        cls='uk-card uk-card-default uk-light', style='background-color: #172031;'
+    )
+
+
+def holdings():
+    return Div(
+        Div(
+            Div(
+                Div('Holdings', cls='uk-text-default uk-text-bolder'),
+                Div(
+                    Span(data_uk_icon='icon: table;'),
+                    Span('Filter', cls='uk-margin-small-left'),
+                    cls='uk-text-small uk-flex uk-flex-middle'
+                ),
+                cls='uk-flex uk-flex-between'
+            ),
+            cls='uk-card-header'
+        ),
+        Div(
+            Div(
+                Div(
+                    Table(
+                        Caption('Portfolio Value'),
+                        Tbody(
+                            *[Tr(
+                                Td(style=f'--start: {values[0]}; --end: {values[1]}')
+                            ) for title, values in [
+                                ('Apple Inc. (AAPL)', (0, 0.3)),
+                                ('Tesla Inc. (TSLA)', (0.3, 0.44)),
+                                ('Amazon.com Inc. (AMZN)', (0.44, 0.6)),
+                                ('Microsoft Corp. (MSFT)', (0.6, 0.68)),
+                                ('Alphabet Inc. (GOOGL)', (0.68, 1))
+                            ]]
+                        ),
+                        cls='charts-css pie'
+                    )
+                ),
+                Div(
+                    Ul(
+                        *[Li(title, Span(amount, cls='uk-text-bolder uk-margin-small-left'), cls='uk-text-small')
+                          for title, amount in [
+                              ('Apple Inc. (AAPL)', '$31,885'), ('Tesla Inc. (TSLA)', '$8,994'),
+                              ('Amazon.com Inc. (AMZN)', '$8,994'), ('Microsoft Corp. (MSFT)', '$8,994'),
+                              ('Alphabet Inc. (GOOGL)', '$8,994')
+                          ]],
+                        cls='charts-css legend legend-inline legend-square', style='border: 0'
+                    )
+                ),
+                data_uk_grid=True,
+                cls='uk-grid-divider uk-child-width-expand uk-grid-match uk-grid-small uk-margin-medium-top'
             ),
             cls='uk-card-body'
         ),
@@ -147,10 +301,10 @@ def transactions():
 page = Div(
     Div(
         Div(menu_card()), Div(overview_card()),
-        # Div(portfolio_value_card()),
+        Div(portfolio_value_card(), cls='uk-width-1-2@m'),
         # Div(assets_card()),
         Div(transactions(), cls='uk-width-1-2@m'),
-        # Div(client_insights_card(), cls='uk-width-1-2@m'),
+        Div(holdings(), cls='uk-width-1-2@m'),
         data_uk_grid=True, cls='uk-padding uk-child-width-1-4@m uk-grid-small uk-grid-match uk-flex-right'
     ),
     style='background-color: #091235'
