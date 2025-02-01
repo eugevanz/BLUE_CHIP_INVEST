5b28937a6526651172595275c155b19358e43558
eugenevanzyl <eugevanz@gmail.com>
2025-02-01 20:27:03 +0200

    At last! My the route setup is completed!

--- main.py
+++ main.py
@@ -2,26 +2,312 @@ import asyncio
 import json
 import os
 import smtplib
+from datetime import datetime
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from typing import Optional, List
 
 import numpy as np
 import numpy_financial as npf
-from fastapi import FastAPI, Request, Form
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, Form, Response
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from redis_om import Field, JsonModel, get_redis_connection
+from supabase import create_client
 
-app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
+
+class Account(JsonModel):
+    account_number: str = Field(index=True)
+    account_type: str
+    balance: float = Field(index=True)
+    profile_id: str  # Add profile_id for reference
+    created_at: datetime = Field(index=True)
+    updated_at: Optional[datetime] = Field(index=True)
+
+    class Meta:
+        database = get_redis_connection(url=os.environ.get("REDIS_URL"), decode_responses=True)
+
+
+class ClientGoal(JsonModel):
+    goal_number: str = Field(index=True)
+    goal_type: str
+    target_amount: float
+    current_savings: float
+    target_date: datetime
+    profile_id: str = Field(index=True)
+    created_at: datetime = Field(index=True)
+    updated_at: Optional[datetime] = Field(index=True)
+
+    class Meta:
+        database = get_redis_connection(url=os.environ.get("REDIS_URL"), decode_responses=True)
+
+
+class DividendPayout(JsonModel):
+    account_id: str
+    payout_number: str = Field(index=True)
+    amount: float
+    payment_date: datetime
+    created_at: datetime = Field(index=True)
+
+    class Meta:
+        database = get_redis_connection(url=os.environ.get("REDIS_URL"), decode_responses=True)
+
+
+class Investment(JsonModel):
+    account_id: str
+    inv_number: str = Field(index=True)
+    investment_type: str
+    symbol: str
+    quantity: float
+    purchase_price: float
+    current_price: float
+    purchase_date: datetime
+    created_at: datetime = Field(index=True)
+    updated_at: Optional[datetime] = Field(index=True)
+
+    class Meta:
+        database = get_redis_connection(url=os.environ.get("REDIS_URL"), decode_responses=True)
+
+
+class Transaction(JsonModel):
+    account_id: str
+    txn_number: str = Field(index=True)
+    amount: float
+    txn_type: str
+    description: str
+    created_at: datetime = Field(index=True)
+
+    class Meta:
+        database = get_redis_connection(url=os.environ.get("REDIS_URL"), decode_responses=True)
+
+
 templates = Jinja2Templates(directory="templates")
 
 EMAIL_LOGIN = os.getenv("EMAIL_LOGIN")
 EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 
+SUPABASE_URL = os.environ.get('SUPABASE_URL')
+SUPABASE_KEY = os.environ.get('SUPABASE_KEY')
+SUPABASE_SERVICE_ROLE_KEY = os.environ.get('SUPABASE_SERVICE_ROLE_KEY')
+SUPABASE_PASSWORD = os.environ.get('SUPABASE_PASSWORD')
+supabase_ = create_client(supabase_url=SUPABASE_URL, supabase_key=SUPABASE_KEY)
+supabase_admin = create_client(supabase_url=SUPABASE_URL, supabase_key=SUPABASE_SERVICE_ROLE_KEY)
+
 with open("static/data.json", "r") as file:
     data = json.load(file)
 
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+# ACCOUNT Routes
+
+@app.get("/select_all_from_accounts_where_profile_id/{profile_id}/", response_model=List[Account])
+async def select_all_from_accounts_where_profile_id(profile_id: str):
+    accounts = Account.find(Account.profile_id == profile_id).all()
+    return accounts
+
+
+@app.post("/create_account/", response_class=HTMLResponse)
+async def create_account(profile_id: str = Form(...), account_type: str = Form(...), balance: float = Form(...)):
+    account_number = Account.Meta.database.incr("account_number_counter")
+    account_number = f"ACC{account_number + 100000}"
+
+    new_account = Account(
+        created_at=datetime.today().date().isoformat(),  # You could use a timestamp here
+        profile_id=profile_id,
+        account_number=account_number,
+        account_type=account_type,
+        balance=balance,
+        updated_at=datetime.today().date().isoformat(),  # Same here for updates
+    )
+    new_account.save()
+    return HTMLResponse(content=f"""<p class="uk-text-meta">
+        Account created successfully with account number <span class="uk-text-bold">{account_number}</span>
+    </p>""")
+
+
+@app.post("/delete_account/{account_number}", response_class=HTMLResponse)
+async def delete_account(account_number: str):
+    account = Account.find(Account.account_number == account_number).first()
+    account.delete()
+    return HTMLResponse(content=f"""<p class="uk-text-meta">
+        Account deleted successfully with account number <span class="uk-text-bold">{account_number}</span>
+    </p>""")
+
+
+# CLIENT GOAL Routes
+
+@app.get("/select_all_from_client_goals_where_profile_id/{profile_id}/")
+async def select_all_from_client_goals_where_profile_id(profile_id: str):
+    goals = ClientGoal.find(ClientGoal.profile_id == profile_id).all()
+    return goals
+
+
+@app.post("/create_client_goal/", response_class=HTMLResponse)
+async def create_client_goal(
+        profile_id: str = Form(...), goal_type: str = Form(...), target_amount: float = Form(...),
+        current_savings: float = Form(...), target_date: str = Form(...)
+):
+    goal_number = ClientGoal.Meta.database.incr("goal_number_counter")
+    goal_number = f"GL{goal_number + 100000}"
+
+    new_client_goal = ClientGoal(
+        created_at=datetime.today().date().isoformat(),  # You could use a timestamp here
+        profile_id=profile_id,
+        goal_type=goal_type,
+        goal_number=goal_number,
+        target_amount=target_amount,
+        current_savings=current_savings,
+        target_date=datetime.strptime(target_date, "%Y-%m-%d").date(),
+        updated_at=datetime.today().date().isoformat(),  # Same here for updates
+    )
+
+    new_client_goal.save()
+    return HTMLResponse(content=f"""<p class="uk-text-meta">
+        Client Goal created successfully for <span class="uk-text-bold">{goal_type}</span>
+    </p>""")
+
+
+@app.post("/delete_client_goal/{goal_number}/", response_class=HTMLResponse)
+async def delete_client_goal(goal_number: str):
+    goal = ClientGoal.find(ClientGoal.goal_number == goal_number).first()
+    goal.delete()
+    return HTMLResponse(content=f"""<p class="uk-text-meta">
+        Client Goal deleted successfully for goal <span class="uk-text-bold">{goal_number}</span>
+    </p>""")
+
+
+# DIVIDENDS AND PAYOUTS Routes
+
+@app.get("/select_all_from_dividends_payouts_where_account/{account_number}/")
+async def select_all_from_dividends_payouts_where_account(account_number: str):
+    account = Account.find(Account.account_number == account_number).first()
+    payouts = DividendPayout.find(DividendPayout.account_id == account.pk).all()
+    return payouts
+
+
+@app.post("/create_dividend_payout/", response_class=HTMLResponse)
+async def create_dividend_payout(
+        profile_id: str = Form(...), account_id: str = Form(...), amount: float = Form(...),
+        payment_date: str = Form(...)
+):
+    payout_number = DividendPayout.Meta.database.incr("payout_number_counter")
+    payout_number = f"DIV{payout_number + 100000}"
+
+    new_dividend_payout = DividendPayout(
+        payout_number=payout_number,
+        created_at=datetime.today().date().isoformat(),  # You could use a timestamp here
+        profile_id=profile_id,
+        account_id=account_id,
+        amount=amount,
+        payment_date=payment_date
+    )
+
+    new_dividend_payout.save()
+    return HTMLResponse(content=f"""<p class="uk-text-meta">
+        Dividend/Payout created successfully for dividend/payout <span class="uk-text-bold">{payout_number}</span>
+    </p>""")
+
+
+@app.post("/delete_dividend_payout/{payout_number}/", response_class=HTMLResponse)
+async def delete_dividend_payout(payout_number: str):
+    payout = DividendPayout.find(DividendPayout.payout_number == payout_number).first()
+    payout.delete()
+    return HTMLResponse(content=f"""<p class="uk-text-meta">
+        Dividend/Payout deleted successfully for <span class="uk-text-bold">{payout_number}</span>
+    </p>""")
+
+
+# INVESTMENTS Routes
+
+@app.get("/select_all_from_investments_where_account/{account_number}/")
+async def select_all_from_investments_where_account_id(account_number: str):
+    account = Account.find(Account.account_number == account_number).first()
+    investments = DividendPayout.find(DividendPayout.account_id == account.pk).all()
+    return investments
+
+
+@app.post("/create_investment/")
+async def create_investment(
+        account_id: str = Form(...), investment_type: str = Form(...),
+        symbol: str = Form(...), quantity: float = Form(...), purchase_price: float = Form(...),
+        current_price: float = Form(...), purchase_date: str = Form(...)
+):
+    inv_number = Investment.Meta.database.incr("inv_number_counter")
+    inv_number = f"INV{inv_number + 100000}"
+
+    new_investment = Investment(
+        inv_number=inv_number,
+        created_at=datetime.today().date().isoformat(),
+        account_id=account_id,
+        investment_type=investment_type,
+        symbol=symbol,
+        quantity=quantity,
+        purchase_price=purchase_price,
+        current_price=current_price,
+        purchase_date=purchase_date,
+        updated_at=datetime.today().date().isoformat()
+    )
+
+    new_investment.save()
+    return HTMLResponse(content=f"""<p class="uk-text-meta">
+        Investments created successfully for investment <span class="uk-text-bold">{inv_number}</span>
+    </p>""")
+
+
+@app.post("/delete_investment/{inv_number}/")
+async def delete_investment(inv_number: str):
+    investment = Investment.find(Investment.inv_number == inv_number).first()
+    investment.delete()
+    return HTMLResponse(content=f"""<p class="uk-text-meta">
+        Investment deleted successfully for <span class="uk-text-bold">{inv_number}</span>
+    </p>""")
+
+
+# TRANSACTIONS Routes
+
+@app.get("/select_all_from_transactions_where_account/{account_number}/")
+async def select_all_from_transactions_where_account_id(account_number: str):
+    account = Account.find(Account.account_number == account_number).first()
+    transactions = DividendPayout.find(DividendPayout.account_id == account.pk).all()
+    return transactions
+
+
+@app.post("/create_transaction/")
+async def create_transaction(
+        account_id: str = Form(...), amount: float = Form(...), txn_type: str = Form(...),
+        description: str = Form(...)
+):
+    txn_number = Transaction.Meta.database.incr("txn_number_counter")
+    txn_number = f"TXN{txn_number + 100000}"
+
+    new_transaction = Transaction(
+        txn_number=txn_number,
+        created_at=datetime.today().date().isoformat(),
+        account_id=account_id,
+        txn_type=txn_type,
+        amount=amount,
+        description=description
+    )
+
+    new_transaction.save()
+    return HTMLResponse(content=f"""<p class="uk-text-meta">
+        Transaction created successfully for transaction <span class="uk-text-bold">{new_transaction}</span>
+    </p>""")
+
+
+@app.post("/delete_transaction/{txn_number}/")
+async def delete_from_transactions(txn_number: str):
+    transaction = Transaction.find(Transaction.txn_number == txn_number).first()
+    transaction.delete()
+    return HTMLResponse(content=f"""<p class="uk-text-meta">
+        Transaction deleted successfully for <span class="uk-text-bold">{txn_number}</span>
+    </p>""")
+
+
+# ------------------------------------------------
 
 @app.get("/", response_class=HTMLResponse)
 async def get(request: Request):
@@ -93,7 +379,6 @@ async def get(request: Request):
 
 
 # Financial Tools
-# ---------------
 
 @app.post("/calculate-adjusted-roi/", response_class=HTMLResponse)
 async def calculate_adjusted_roi(
@@ -447,7 +732,6 @@ async def calculate_simple_interest(principal: int = Form(0), rate: int = Form(0
 
 
 # SENDING EMAILS
-# --------------
 
 async def send_email(name: str, sender_email: str, message: str):
     msg = MIMEMultipart()
@@ -479,3 +763,122 @@ async def post(name: str = Form(...), email: str = Form(...), message: str = For
     <p class="uk-text-lead">Thank you for reaching out to Blue Chip Investments! We appreciate your interest and the 
     opportunity to help you navigate the world of smart investing.</p>
     </article>""")
+
+
+# LOGGED IN USER
+
+@app.get("/login/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(request=request, name="administrative/login.html")
+
+
+@app.get("/admin/", response_class=HTMLResponse)
+async def admin(request: Request):
+    profile_data = {
+        "profile_picture_url": request.cookies.get("profile_picture_url"),
+        "first_name": request.cookies.get("first_name"),
+        "last_name": request.cookies.get("last_name"),
+        "email": request.cookies.get("email")
+    }
+    return templates.TemplateResponse(request=request, name="admin.html", context=profile_data)
+
+
+@app.get("/client/", response_class=HTMLResponse)
+async def client(request: Request):
+    profile_data = {
+        "profile_picture_url": request.cookies.get("profile_picture_url"),
+        "first_name": request.cookies.get("first_name"),
+        "last_name": request.cookies.get("last_name"),
+        "email": request.cookies.get("email")
+    }
+    return templates.TemplateResponse(request=request, name="client.html", context=profile_data)
+
+
+# LOGON
+
+@app.get("/sign_out/")
+async def sign_out(response: Response):
+    supabase_admin.auth.sign_out()
+    response.delete_cookie("profile_picture_url")
+    response.delete_cookie("first_name")
+    response.delete_cookie("last_name")
+    response.delete_cookie("profile_type")
+    response.delete_cookie("email")
+    response.delete_cookie("access_token")
+
+    return RedirectResponse("/")
+
+
+@app.post("/request_otp/", response_class=HTMLResponse)
+async def request_otp(email: str = Form(...)):
+    response = supabase_admin.auth.sign_in_with_otp({"email": email, "options": {"should_create_user": False}})
+
+    if response and response.user is None:
+        return HTMLResponse(content=f"""<form action="/verify_otp/" method="post" class="uk-margin">
+            <h3 class="uk-card-title uk-text-bolder uk-margin-remove-bottom">Ready to sign-in?</h3>
+            <p class="uk-text-small uk-margin-remove-top" style="color: #091235;">
+                Please enter the <strong>verification code</strong> that was sent to your email. This code is required
+                to verify your identity and complete the login process.
+            </p>
+            <div class="uk-text-small">One-time PIN</div>
+            <div class="uk-inline">
+                <span class="uk-form-icon" uk-icon="icon: lock"></span>
+                <input class="uk-input uk-form-width-large" name="sent_code" type="text">
+            </div>
+            <input name="sent_email" type="hidden" value="{email}" required>
+            <p class="uk-text-meta">Please enter your OTP and click Sign In.</p>
+            <button class="uk-button uk-button-large uk-light" style="background-color:#091235;">Sign In</button>
+        </form>""")
+    else:
+        return HTMLResponse(
+            content=f"<p class='uk-text-danger' id='send_code_notifications'>{response.error_message}</p>")
+
+
+@app.post("/verify_otp/", response_class=HTMLResponse)
+async def verify_otp(response: Response, sent_email: str = Form(...), sent_code: str = Form(...)):
+    auth_response = supabase_admin.auth.verify_otp({"email": sent_email, "token": sent_code, "type": "email"})
+
+    if auth_response and auth_response.user:
+        profile_response = supabase_.table("profiles").select("*").eq("id", auth_response.user.id).single().execute()
+        profile = profile_response.data[0]
+        response.set_cookie(key="profile_picture_url", value=profile['profile_picture_url'])
+        response.set_cookie(key="first_name", value=profile['first_name'])
+        response.set_cookie(key="last_name", value=profile['last_name'])
+        response.set_cookie(key="profile_type", value=profile['profile_type'])
+        response.set_cookie(key="email", value=profile['email'])
+        response.set_cookie(key="access_token", value=auth_response.session.access_token, httponly=True, secure=True)
+
+        return RedirectResponse("/admin/") if profile["profile_type"] == "admin" else RedirectResponse("/client/")
+    else:
+        RedirectResponse("/login/")
+
+
+# PROFILES Routes
+
+@app.post("/update_profile/", response_class=HTMLResponse)
+def update_profile(
+        profile_id: str = Form(...), profile_picture_url: str = Form(...), first_name: str = Form(...),
+        last_name: str = Form(...)
+):
+    update_data = {}
+    if profile_picture_url is not None:
+        update_data["profile_picture_url"] = profile_picture_url
+    if first_name is not None:
+        update_data["first_name"] = first_name
+    if last_name is not None:
+        update_data["last_name"] = last_name
+
+    response = supabase_.table("profiles").update(update_data).eq("id", profile_id).execute()
+    if response.error:
+        return HTMLResponse(content=f"""<p>Failed to update profile: {response.error_message}</p>""")
+    if response.data:
+        return HTMLResponse(content=f"""<p>Profile updated successfully.</p>""")
+
+
+@app.post("/send_invite/", response_class=HTMLResponse)
+def send_invite(email: str = Form(...)):
+    response = supabase_admin.auth.admin.invite_user_by_email(email)
+    if response and response.user:
+        return HTMLResponse(content=f"""<p>Invite sent to {response.user.email}</p>""")
+    else:
+        return HTMLResponse(content=f"""<p>Error sending invite: {response["error"]["message"]}</p>""")
